0e4186327fc945dab4b60aeb523eed39573f60b6
Kolton Quinton <kolton.quinton@gmail.com>
2022-02-22 22:48:02 -0600

    working so far

--- Ind02_Quinton_Kolton/ViewController.swift
+++ Ind02_Quinton_Kolton/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-/*
+    
     var PuzzleImageTest: [UIImage] = [
         UIImage(named: "6090663.png")!,
         UIImage(named: "akali-0-1.png")!,
@@ -39,14 +39,6 @@ class ViewController: UIViewController {
         UIImage(named: "akali-3-4.png")!
     ]
  
-    var PuzzleImage: [[UIImage]] = [
-        [UIImage(named: "6090663.png")!, UIImage(named: "akali-1-0.png")!, UIImage(named: "akali-2-0.png")!,UIImage(named: "akali-3-0.png")!],
-        [UIImage(named: "akali-0-1.png")!, UIImage(named: "akali-1-1.png")!, UIImage(named: "akali-2-1.png")!,UIImage(named: "akali-3-1.png")!],
-        [UIImage(named: "akali-0-2.png")!, UIImage(named: "akali-1-2.png")!, UIImage(named: "akali-2-2.png")!,UIImage(named: "akali-3-2.png")!],
-        [UIImage(named: "akali-0-3.png")!, UIImage(named: "akali-1-3.png")!, UIImage(named: "akali-2-3.png")!,UIImage(named: "akali-3-3.png")!],
-        [UIImage(named: "akali-0-4.png")!, UIImage(named: "akali-1-4.png")!, UIImage(named: "akali-2-4.png")!,UIImage(named: "akali-3-4.png")!]
-    ]
- */
 
     @IBAction func ShuffleButton(_ sender: UIButton) {
     }
